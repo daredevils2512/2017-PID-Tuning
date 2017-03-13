3d8b27e151259373275bb5643cd33397f7495af2
Daredevils <daredevils2512@gmail.com>
2017-03-13 18:24:29 -0500

    Progress towards PID button setup. Not working, needs fixes yet.

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,11 +1,19 @@
 #include "OI.h"
 
 #include <WPILib.h>
+#include "Commands/UpdatePIDValue.h"
+#include "Commands/UpdateLastPressedPointer.h"
 
 OI::OI() {
 	std::cout << "OI Init" << std::endl;
 	//driverController.reset(new Joystick(0));
-
+	DRC_leftBumper.WhenReleased(new UpdatePIDValue(&incrementValue, 0.1, true));
+	DRC_rightBumper.WhenReleased(new UpdatePIDValue(&incrementValue, 10, true));
+	DRC_yButton.WhenReleased(new UpdateLastPressedPointer(&valueP)); // P
+	DRC_xButton.WhenReleased(new UpdateLastPressedPointer(&valueI)); // I
+	DRC_bButton.WhenReleased(new UpdateLastPressedPointer(&valueD)); // D
+	DRC_aButton.WhenReleased(new UpdateLastPressedPointer(&valueF)); // F
+	DRC_POV.WhenPressed(new UpdatePIDValue(toModify,incrementValue,false));
 	// Process operator interface input here.'
 
 }
--- src/OI.h
+++ src/OI.h
@@ -3,14 +3,14 @@
 
 #include "WPILib.h"
 #include "TriggerButton.h"
-
+#include "POVButton.h"
 
 //creates all of the inputs on the controllers
 class OI
 {
 private:
 	//declares controllers
-	Joystick driverController{0};
+
 	//Joystick driverController{0};
 	TriggerButton DRC_rightTrigger{&driverController, 3, 0.8};
 	TriggerButton DRC_leftTrigger{&driverController, 2, 0.8};
@@ -21,9 +21,17 @@ private:
 	JoystickButton DRC_xButton{&driverController, 3};
 	JoystickButton DRC_yButton{&driverController, 4};
 	JoystickButton DRC_startButton{&driverController, 8};
+	POVButton DRC_POV{&driverController,0};
 
 
 public:
+	Joystick driverController{0};
+	double* toModify = &valueP;
+	double valueP = 0.003;
+	double valueI = 0.12;
+	double valueD = 1.23;
+	double valueF = 0;
+	double incrementValue = 1;
 	OI();
 	double GetX();
 	double GetY();
